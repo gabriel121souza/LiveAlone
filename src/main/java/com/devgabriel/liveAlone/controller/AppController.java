1aa8ee6aabe81ea64364fd13792f78439ab08412
gabriel121souza <gabriel121souza@gmail.com>
2020-11-29 23:38:56 -0300

    implementado o salvar

--- src/main/java/com/devgabriel/liveAlone/controller/AppController.java
+++ src/main/java/com/devgabriel/liveAlone/controller/AppController.java
@@ -5,7 +5,9 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 import com.devgabriel.liveAlone.model.Produto;
 import com.devgabriel.liveAlone.service.ProdutoService;
@@ -23,4 +25,19 @@ public class AppController {
 		return "index";
 		
 	}
-}
+	//navegando pela pagina new 
+	@RequestMapping("/new")
+	public String showNewProductForm(Model model) {
+		Produto produto = new Produto();
+		model.addAttribute("produto", produto);
+		return "novo_produto";
+	}
+	
+	@RequestMapping(value="/save", method = RequestMethod.POST)
+	public String salvarProduto(@ModelAttribute("produto") Produto produto) {
+		service.save(produto);
+		return "redirect:/";
+		}
+		
+	}
+
